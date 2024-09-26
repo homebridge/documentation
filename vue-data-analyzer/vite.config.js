5b69987a0385963c163a487e5374df98b06c86c0
Northern Man <19808920+NorthernMan54@users.noreply.github.com>
2024-09-26 17:51:09 -0400

    index.html

--- vue-data-analyzer/vite.config.js
+++ vue-data-analyzer/vite.config.js
@@ -5,3 +5,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
 })
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/documentation/' : '/'
+};
